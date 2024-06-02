c5d399fb7437a8fef4473312beefb4b0a7e60010
mr-macintosh01 <65767905+mr-macintosh01@users.noreply.github.com>
2024-06-02 21:17:57 +0200

    Add files via upload

---
+++ html/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test LEMP Stack</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <h1>Test LEMP Stack</h1>
+    <p><a href="db.php">View data from the database</a></p>
+</body>
+</html>
